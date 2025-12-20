f51d60b3c4ffaf2d03a068bf9c390d3e704e41c0
beeni94 <beenisafdar@gmail.com>
2025-12-20 09:27:50 +0500

    Add files via upload

---
+++ server(Conut words).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Linq;
+
+class Server
+{
+    static void Main()
+    {
+        var listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Server is running... waiting for strings.");
+
+        while (true)
+        {
+            using (var client = listener.AcceptTcpClient())
+            using (var ns = client.GetStream())
+            {
+                byte[] data = new byte[4096];
+                int bytesRead = ns.Read(data, 0, data.Length);
+                string receivedText = Encoding.UTF8.GetString(data, 0, bytesRead);
+                Console.WriteLine($"Received: {receivedText}");
+                int wordCount = receivedText.Split(new char[] { ' ', '\t', '\n', '\r' },
+                                 StringSplitOptions.RemoveEmptyEntries).Length;
+
+                string response = $"The string contains {wordCount} words.";
+                byte[] responseData = Encoding.UTF8.GetBytes(response);
+                ns.Write(responseData, 0, responseData.Length);
+                Console.WriteLine($"Sent word count: {wordCount}");
+            }
+        }
+    }
+}
